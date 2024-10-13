f863516cfd24a90b523127238d787ec8d0b6170f
Nhial <>
2024-10-13 14:06:41 +0300

    updated files

---
+++ main.js
@@ -0,0 +1,5 @@
+
+  // Function to display a message when the user clicks the "Shop Now" button
+  function shopNowClick() {
+    alert("Thank you for your interest! Shop Now button clicked.");
+  }
